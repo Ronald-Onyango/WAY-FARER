70078bb7f4f61a233262943a1558b01e0426a647
Ronald Onyango <ronronny432@gmail.com>
2019-07-31 17:37:49 +0300

    feature(routes):setup user routes [Feature #167605225]

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { config } from 'dotenv';
+import apiRoutes from './routes';
 import bodyParser from 'body-parser';
 import 'babel-polyfill';
 
@@ -9,6 +10,7 @@ config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/api', apiRoutes);
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
 });
---
+++ server/src/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const apiRoutes = express.Router();
+
+apiRoutes.get('/', (req, res) => res.json({
+  status: 200,
+  message: 'Welcome to WayFarer API',
+}));
+
+apiRoutes.get('/v1', (req, res) => res.json({
+  status: 200,
+  message: 'Welcome to version 1 of WayFarer API',
+}));
+
+export default apiRoutes;
