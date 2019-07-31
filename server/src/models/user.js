5427e916b32b23693679586eb1638a43d067c8ff
Ronald Onyango <ronronny432@gmail.com>
2019-07-31 22:40:33 +0300

    feature(sign-up):add user signup model [Feature #167605225]

---
+++ server/src/models/user.js
@@ -0,0 +1,31 @@
+
+import moment from 'moment';
+
+class Users {
+  /**
+   * class constructor
+   * @param {object} data
+   */
+  constructor() {
+    this.users = [];
+  }
+  /**
+   *
+   * @returns {object} User object
+   */
+
+  create(data) {
+    const newUser = {
+      id: this.users.len + 1,
+      email: data.email || '',
+      first_name: data.first_name || '',
+      last_name: data.last_name || '',
+      is_admin: data.is_admin || false,
+      createdDate: moment.now(),
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
+
+export default new Users();
