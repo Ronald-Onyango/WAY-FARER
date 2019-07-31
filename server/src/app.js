dce0c2eae74eb75c7d42b07261170f3bbcdcf7ed
Ronald Onyango <ronronny432@gmail.com>
2019-07-31 15:45:27 +0300

    feature(server):setup server [Starts #167605225]

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+import bodyParser from 'body-parser';
+import 'babel-polyfill';
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
+
+export default app;
